7b0816ee7093286784ebe837441b842c10767a30
Alvis Duong <alvis@cloudflare.com>
2020-11-23 22:52:36 +0000

    WASM Prototype

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,12 @@ use hpke::{
     aead::{AeadTag, ChaCha20Poly1305},
     kdf::HkdfSha256,
     kem::X25519HkdfSha256,
-    kex::{Deserializable, KeyExchange},
+    kex::{Deserializable, KeyExchange, Serializable},
     EncappedKey, HpkeError, Kem as KemTrait, OpModeR,
 };
-use rand::{rngs::StdRng, SeedableRng};
+use rand::{rngs::StdRng, rngs::OsRng, SeedableRng};
 use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::*;
 
 type Kem = X25519HkdfSha256;
 type Aead = ChaCha20Poly1305;
@@ -15,6 +16,44 @@ type Kdf = HkdfSha256;
 type Kex = <Kem as KemTrait>::Kex;
 
 const ENCRYPTION_FORMAT_VERSION: u8 = 1;
+
+#[wasm_bindgen]
+pub fn decrypt(private_key: &str, matched_data: &str) -> String {
+    let private_key_bytes = radix64::STD
+        .decode(&private_key)
+        .expect("Cannot decode private key");
+    let private_key =
+        get_private_key_from_bytes(&private_key_bytes).expect("Failed to get private key");
+
+    let encrypted_matched_data_bytes = radix64::STD
+        .decode(&matched_data)
+        .expect("Cannot decode matched data");
+    let encrypted_matched_data = deserialize_encrypted_data(&encrypted_matched_data_bytes)
+        .expect("Deserializing encrypted data failed");
+
+    let matched_data =
+        decrypt_data(&encrypted_matched_data, &private_key).expect("Failed to decrypt");
+
+    return String::from_utf8_lossy(&matched_data).to_string();
+}
+
+#[derive(Serialize, Deserialize)]
+struct KeyPair {
+    private_key: String,
+    public_key: String,
+}
+
+#[wasm_bindgen]
+pub fn keypair() -> JsValue {
+    let (private_key, public_key) = generate_key_pair();
+
+    let key_pair = KeyPair {
+        private_key: radix64::STD.encode(&private_key.to_bytes()),
+        public_key: radix64::STD.encode(&public_key.to_bytes()),
+    };
+
+    JsValue::from_serde(&key_pair).unwrap()
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct EncryptedData {
